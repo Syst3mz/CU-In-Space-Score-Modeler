bd01c000e472ca280d27ac246116c16ac766ac72
Ethan Ansburg <ethanansburgstda@gmail.com>
2024-09-04 20:39:53 -0600

    Added support for configuration.

---
+++ src/config.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    #[serde(default)]
+    pub golf_ball_mass_kg: f32,
+
+    #[serde(default)]
+    pub dead_mass_kilo_grams: f32,
+
+    #[serde(default)]
+    pub max_impulse: u16,
+
+    #[serde(default)]
+    pub max_golf_balls: u16,
+
+    #[serde(default)]
+    pub gravity: f32,
+
+    #[serde(default)]
+    pub max_tracked_solutions: usize
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            golf_ball_mass_kg: 0.04592623,
+            dead_mass_kilo_grams: 4.0,
+            max_impulse: 5120,
+            max_golf_balls: 1000,
+            gravity: 9.81,
+            max_tracked_solutions: 50,
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 mod scoring_criteria;
+mod config;
+
+use std::io::Write;
 use crate::scoring_criteria::ScoringCriteria;
-
-const GOLF_BALL_MASS_KILOGRAMS: f32 = 0.04592623;
-
-/// Mass of everything that is not payload on the launchpad.
-const DEAD_MASS_KILO_GRAMS: f32 = 4.0;
-const MAX_IMPULSE: u16 = 5120;
-const MAX_GOLF_BALLS: u16 = 1000;
-const GRAVITY: f32 = 9.81;
-
-/// How many of the top solutions should be kept.
-const MAX_TRACKED_SOLUTIONS: usize = 50;
+use crate::config::Config;
 
 /// Assuming $v_0 = 0$, and that the entire impulse is applied instantly, the velocity on the
 /// launchpad is (from https://www.calculatorsoup.com/calculators/physics/impulse-momentum.php)
@@ -29,36 +22,68 @@ fn compute_initial_velocity(mass: f32, impulse: f32) -> f32 {
 /// $at = u$
 /// $t = \frac{u}{a}$
 /// from: https://www.quora.com/A-ball-is-thrown-vertically-upward-with-a-speed-of-20-m-s-When-will-it-reach-the-maximum-height-What-is-the-maximum-height-reached
-fn compute_time_of_apogee(initial_velocity: f32) -> f32 {
-    initial_velocity / GRAVITY
+fn compute_time_of_apogee(initial_velocity: f32, gravity: f32) -> f32 {
+    initial_velocity / gravity
 }
 
 /// Then we can use the $t$ found above in the following equation to compute apogee.
 /// $h = ut+\frac{1}{2}at^2$
 /// /// from: https://www.quora.com/A-ball-is-thrown-vertically-upward-with-a-speed-of-20-m-s-When-will-it-reach-the-maximum-height-What-is-the-maximum-height-reached
-fn compute_apogee(initial_velocity: f32, time_of_apogee: f32) -> f32 {
+fn compute_apogee(initial_velocity: f32, time_of_apogee: f32, gravity: f32) -> f32 {
     // powi is raise the preceding f32 to an integer power, in this case 2.
-    initial_velocity * time_of_apogee + (0.5 * -GRAVITY * time_of_apogee.powi(2))
+    initial_velocity * time_of_apogee + (0.5 * -gravity * time_of_apogee.powi(2))
 }
 
-fn simulate_rocket_apogee(mass: f32, impulse: f32) -> f32 {
+fn simulate_rocket_apogee(mass: f32, impulse: f32, gravity: f32) -> f32 {
     let initial_velocity = compute_initial_velocity(mass, impulse);
-    let time_of_apogee = compute_time_of_apogee(initial_velocity);
-    compute_apogee(initial_velocity, time_of_apogee)
+    let time_of_apogee = compute_time_of_apogee(initial_velocity, gravity);
+    compute_apogee(initial_velocity, time_of_apogee, gravity)
 }
 
-// test every possible combination of impulse and golf balls.
+
+const CONFIG_PATH: &'static str = "config.json";
+/// Read in a config JSON file.
+fn read_in_config() -> anyhow::Result<Config> {
+    let json_text = std::fs::read_to_string(CONFIG_PATH)?;
+    Ok(serde_json::from_str::<Config>(&json_text)?)
+}
+
+fn write_config_to_file(config: &Config) -> anyhow::Result<()> {
+    let mut file = std::fs::File::create(CONFIG_PATH)?;
+    let text = serde_json::to_string_pretty(config)?;
+    file.write_all(text.as_bytes())?;
+    Ok(())
+}
+fn get_config() -> Config {
+    read_in_config().unwrap_or_else(|e| {
+        println!("{}", e);
+        println!("Unable to read in config file due to above error. A new one will be generated and written to disk.");
+        let config = Config::default();
+
+        write_config_to_file(&config).unwrap_or_else(|e| {
+            println!("{}", e);
+            println!("Unable to write out config file due to above error. Program will proceed with defaults.");
+            println!("Default configuration:\n{:#?}", &config)
+        });
+
+        config
+    })
+}
+
 fn main() {
+    let config = get_config();
+
     let mut best = vec![];
 
     print!("Sweeping...");
-    for max_impulse in 0..MAX_IMPULSE {
-        for golf_balls in 0..MAX_GOLF_BALLS {
-            let mass = (GOLF_BALL_MASS_KILOGRAMS * golf_balls as f32) + DEAD_MASS_KILO_GRAMS;
+    // test every possible combination of impulse and golf balls.
+    for max_impulse in 0..config.max_impulse {
+        for golf_balls in 0..config.max_golf_balls {
+            let mass = (config.golf_ball_mass_kg * golf_balls as f32) + config.dead_mass_kilo_grams;
             let impulse = max_impulse as f32;
 
             let setup = ScoringCriteria::new(
-                simulate_rocket_apogee(mass, impulse),
+                simulate_rocket_apogee(mass, impulse, config.gravity),
                 golf_balls,
                 impulse,
             );
@@ -71,7 +96,7 @@ fn main() {
             let score = setup.score();
             best.push((setup, score));
 
-            if best.len() > MAX_TRACKED_SOLUTIONS {
+            if best.len() > config.max_tracked_solutions {
                 // Take out the worst tracked solution.
                 best.sort_by(|(_, a), (_,b)| b.total_cmp(a));
                 best.pop();
@@ -91,19 +116,19 @@ mod tests {
 
     #[test]
     fn time_of_apogee() {
-        let time = compute_time_of_apogee(20.0);
+        let time = compute_time_of_apogee(20.0, 9.81);
         assert_eq!(time, 2.0387359)
     }
 
     #[test]
     fn height_of_apogee() {
-        let height = compute_apogee(20.0, 2.04);
+        let height = compute_apogee(20.0, 2.04, 9.81);
         assert_eq!(height, 20.387352)
     }
 
     #[test]
     fn rocket_apogee() {
-        let height = simulate_rocket_apogee(2.0, 40.0);
+        let height = simulate_rocket_apogee(2.0, 40.0, 9.81);
         assert_eq!(height, 20.38736)
     }
 }
